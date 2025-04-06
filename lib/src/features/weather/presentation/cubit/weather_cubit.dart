2ecd3fdedfe57747c0aa8f7699e0405f0c52e2ec
ABHISHEK <iabhishekmp@gmail.com>
2025-04-06 16:16:05 +0530

    feat: Add initial structure for weather feature components

---
+++ lib/src/features/weather/presentation/cubit/weather_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'weather_state.dart';
+
+class WeatherCubit extends Cubit<WeatherState> {
+  WeatherCubit() : super(WeatherInitial());
+}
